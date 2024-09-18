eb99d8a795e5611f0b862faeb8946db77d8df0bd
Eksaq Dev <rishith.saginala@eksaq.in>
2024-09-18 12:37:00 +0530

    hello

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <h1>Counter App</h1>
+      <h1>Counting App</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
